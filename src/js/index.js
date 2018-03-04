a6f6d2634e49b6bc4cb89fa782c5872f0b2f9c6a
Henry Desroches <yo@henry.codes>
2018-03-04 10:58:55 -0700

    Calc height if mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,28 @@
-import Pjax from './modules/Pjax';
-import threeStuff from './modules/threeStuff';
-import backgroundAnimation from './modules/backgroundAnimation';
-import overlay from './modules/overlay';
+import Pjax from "./modules/Pjax";
+import threeStuff from "./modules/threeStuff";
+import backgroundAnimation from "./modules/backgroundAnimation";
+import overlay from "./modules/overlay";
 
 threeStuff();
 Pjax();
 backgroundAnimation();
 overlay();
 
-window.addEventListener('resize', () => {
-    backgroundAnimation();
-})
+const isMobileDevice = () =>
+  typeof window.orientation !== "undefined" ||
+  navigator.userAgent.indexOf("IEMobile") !== -1;
+
+window.addEventListener("resize", () => {
+  backgroundAnimation();
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+  if (isMobileDevice()) {
+    const wrapper = document.querySelector("#wrapper");
+    const height = document.documentElement.clientHeight;
+    const canvases = document.querySelectorAll("canvas");
+
+    wrapper.style.height = `${height - 50}px`;
+    canvases.forEach(canvas => (canvas.height = height));
+  }
+});
