457f3ba3e08fe48dcd9b85e70c725333e820973b
Henry Desroches <yo@henry.codes>
2018-03-04 10:34:10 -0700

    Adds npm script, updates busts appearance, updates transition object, removes background from threejs.

--- src/js/modules/Pjax.js
+++ src/js/modules/Pjax.js
@@ -1,38 +1,43 @@
 import Barba from "barba.js";
 import backgroundAnimation from "./backgroundAnimation";
-import overlay from './overlay';
+import overlay from "./overlay";
 
 const Pjax = () => {
-Object.assign(Barba.Pjax.Dom, {
-  containerClass: "inner",
-  wrapperId: "wrapper"
-});
-const HideShowTransition = Barba.BaseTransition.extend({
-  start: function() {
-    this.newContainerLoading.then(this.transitionOut.bind(this));
-  },
-  transitionOut: function() {
-    backgroundAnimation(this.newContainer, this.oldcontainer);
-    this.newContainer.classList.remove("active");
-    this.oldContainer.classList.remove("active");
-    setTimeout(() => {
-      this.finish();
-    }, 200);
-  },
-  finish: function() {
-    overlay(this.newContainer);
-    document.body.scrollTop = 0;
-    this.newContainer.classList.add("active");
-    this.newContainer.style.visibility = "visible";
-    this.done();
-  }
-});
-Barba.Pjax.getTransition = () => HideShowTransition;
-document.addEventListener("DOMContentLoaded", event => {
-  Barba.Pjax.start();
-  Barba.Prefetch.init();
-});
-
-}
+  Object.assign(Barba.Pjax.Dom, {
+    containerClass: "inner",
+    wrapperId: "wrapper"
+  });
+  const HideShowTransition = Barba.BaseTransition.extend({
+    start: function() {
+      this.newContainerLoading.then(this.transitionOut.bind(this));
+    },
+    transitionOut: function() {
+      const wrapper = document.querySelector("#wrapper");
+      if (window.location.pathname === "/") {
+        wrapper.classList.add("home");
+      } else {
+        wrapper.classList.remove("home");
+      }
+      backgroundAnimation(this.newContainer, this.oldcontainer);
+      this.newContainer.classList.remove("active");
+      this.oldContainer.classList.remove("active");
+      setTimeout(() => {
+        this.finish();
+      }, 200);
+    },
+    finish: function() {
+      overlay(this.newContainer);
+      document.body.scrollTop = 0;
+      this.newContainer.classList.add("active");
+      this.newContainer.style.visibility = "visible";
+      this.done();
+    }
+  });
+  Barba.Pjax.getTransition = () => HideShowTransition;
+  document.addEventListener("DOMContentLoaded", event => {
+    Barba.Pjax.start();
+    Barba.Prefetch.init();
+  });
+};
 
 export default Pjax;
